61d5bfe3f0d474eb60f7e3edb15a9400cf4fed5e
nicolas.richasse <nicolas.richasse@f2a43027-3046-0410-912d-c7ebed2548ad>
2008-02-15 12:21:42 +0000

    JPerf 1.0 folder deleted

--- trunk/jperf-1.0/jsrc/JperfWindowListen.java
+++
@@ -1,9 +0,0 @@
-import java.awt.event.*;
-
-public class JperfWindowListen extends WindowAdapter{
-  public JperfWindowListen() {};
-  
-  public void windowClosing(WindowEvent e) {
-    System.exit(0);
-  }
-}
